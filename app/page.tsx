a315ccf45be7e5108c34a193878b0e1ab3420e32
Rike-Benjamin Schuppner <debilski@debilski.de>
2026-04-16 23:12:14 +0200

    DOC: Intro text

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,25 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main
-      className={`min-h-screen flex-col items-center justify-between px-24 py-12`}>
-      <div className="z-10 w-full max-w-screen items-center justify-between font-mono text-sm text-white">
-          <div>
-            <Link href="tournament">New Tournament</Link>
-          </div>
-          <div>
-            <Link href="tournament-history">Tournament history</Link>
-          </div>
-          <div>
-            <Link href="demo">Demos</Link>
-          </div>
+    <main className={`min-h-screen flex-col items-center justify-between px-24 py-12`}>
+      <div className="z-10 w-full max-w-screen items-center justify-between font-mono text-sm text-amber-100">
+        <h1>Pelita tournament web viewer.</h1>
+        <div className="mt-4">
+          <Link href="tournament">New Tournament</Link> <sup>*</sup>
+        </div>
+        <div>
+          <Link href="tournament-history">Tournament history</Link>
+        </div>
+        <div>
+          <Link href="demo">Demos</Link>
+        </div>
+        <div className="mt-8">
+          <hr />
+          <small>
+            <sup>*</sup> Please note: Viewing a live tournament is only possible when run with a
+            proper server. Not on Github. Instructions will follow.
+          </small>
+        </div>
       </div>
     </main>
   );
